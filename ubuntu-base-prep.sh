f78d87c2366344947fb2b3a991c5f03d520031a5
Clay Chang <clay.chang@gmail.com>
2024-08-16 09:25:50 +0000

    feat(ubuntu): upgrade ubuntu to 20.04.5

--- ubuntu-base-prep.sh
+++ ubuntu-base-prep.sh
@@ -21,8 +21,8 @@ echo $PROXY
 sudo apt install qemu-user-static
 
 # Download Ubuntu Base file system (https://wiki.ubuntu.com/Base)
-ROOTFSURL=http://cdimage.ubuntu.com/ubuntu-base/releases/18.04.3/release/
-ROOTFS=ubuntu-base-18.04.3-base-${ARCH}.tar.gz
+ROOTFSURL=https://cdimage.ubuntu.com/ubuntu-base/releases/20.04.5/release/
+ROOTFS=ubuntu-base-20.04.5-base-${ARCH}.tar.gz
 if [ ! -s ${ROOTFS} ]; then
     curl -OLf ${ROOTFSURL}${ROOTFS}
 fi
